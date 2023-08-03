82f17ae647083db4a70d7b67aedf3fd7daf040d5
cala18 <brayancala2375@gmail.com>
2023-08-03 17:41:33 -0500

    feat: :zap: funcionalidad navbar

--- js/main.js
+++ js/main.js
@@ -1,9 +1,26 @@
-let menu =document.querySelector("#menu-icon");
+let menu = document.querySelector("#menu-icon");
 let navbar = document.querySelector(".navbar");
 
-menu.addEventListener("click", () => {
+menu.addEventListener("click", function () {
     navbar.classList.toggle("active");
 });
 window.onscroll = () => {
     navbar.classList.remove("active");
-}
+};
+
+// // JavaScript
+// let menu = document.querySelector("#menu-icon");
+// let navbar = document.querySelector(".navbar");
+// let menuOpen = false;// Variable para rastrear el estado del menú
+
+// menu.addEventListener("click", function () {
+//   navbar.classList.toggle(".active");
+//   menuOpen = !menuOpen; // Cambiar el estado del menú al hacer clic
+// });
+
+// window.onscroll = () => {
+//   if (!menuOpen) {
+//     // Solo si el menú no está abierto, entonces remueve la clase "active"
+//     navbar.classList.remove(".active");
+//   }
+// };
